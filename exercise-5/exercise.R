5cc88020c92236fd45ea166b995345a8a4c3b10b
Karlpa <karlpa@uw.edu>
2016-11-17 16:15:52 -0800

    Uploaded today

--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -7,9 +7,12 @@ library(dplyr)
 
 # In which month was the average departure delay the greatest?
 # Hint: you'll have to perform a grouping operation before summarizing your data
-
-
-# If you create a data.frame with the columns "month", and "delay" above, you should be able to create 
+visual <- flights %>%
+  group_by(month) %>% 
+  summarise(delay = mean(arr_delay, na.rm = TRUE)) %>% 
+  arrange(-delay)
+plot(visual)
+# If you create a data.frame with the columns "month", and "delay" above, you should be able to create
 # a scatterplot by passing it to the 'plot' function
 
 
--- exercise-6/exercise.R
+++ exercise-6/exercise.R
@@ -8,7 +8,7 @@ library(dplyr)
 
 # Create a dataframe of the average arrival delay for each destination, then use left_join
 # to join on the "airports" dataframe, which has the airport info
-
+average.delays <- select(flights, dest, arr_delay)
 
 # Create a dataframe of the average arrival delay for each airline, then use left_join
 # to join on the "airlines" dataframe, which has the airline info
